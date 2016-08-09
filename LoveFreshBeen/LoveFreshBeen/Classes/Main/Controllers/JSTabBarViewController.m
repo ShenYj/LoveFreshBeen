bb2e75fac425da6301abfd9a06226cc4954f8ab2
ShenYj <shenyanjie123@foxmail.com>
2016-08-09 09:20:55 +0800

    设置程序图标

---
+++ LoveFreshBeen/LoveFreshBeen/Classes/Main/Controllers/JSTabBarViewController.m
@@ -0,0 +1,50 @@
+//
+//  JSTabBarViewController.m
+//  LoveFreshBeen
+//
+//  Created by ShenYj on 16/8/9.
+//  Copyright © 2016年 ___ShenYJ___. All rights reserved.
+//
+
+#import "JSTabBarViewController.h"
+#import "JSNavigationController.h"
+
+@interface JSTabBarViewController ()
+
+@end
+
+@implementation JSTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+/**
+ *  添加子控制器
+ *
+ *  @param viewcontroller 子控制器
+ *  @param title          子控制器标题
+ *  @param imageName      自控制提图标
+ */
+- (void)addChildViewControllers:(UIViewController *)viewcontroller withTitle:(NSString *)title withImageName:(NSString *)imageName{
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
